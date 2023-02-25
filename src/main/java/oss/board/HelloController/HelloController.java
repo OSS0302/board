3ad6b0d126cfc81de425f9c810ac0af1335167c9
ohsangsoo <tkdtn3264@gmail.com>
2023-02-25 20:54:20 +0900

    welcome 동적페이지 만들기

--- src/main/java/oss/board/HelloController/HelloController.java
+++ src/main/java/oss/board/HelloController/HelloController.java
@@ -1,10 +1,18 @@
 package oss.board.HelloController;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 
-
+@Controller
 public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","spring!!");
+        return "hello";
+    }
 
 }
