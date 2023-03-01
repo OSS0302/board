52be3cd83561abbfd56068eb34a430625eec8ff6
ohsangsoo <tkdtn3264@gmail.com>
2023-03-01 21:09:13 +0900

    HttpMessageConverter(jsonConverter) api 실습하기

--- src/main/java/oss/board/HelloController/HelloController.java
+++ src/main/java/oss/board/HelloController/HelloController.java
@@ -3,7 +3,6 @@ package oss.board.HelloController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -22,10 +21,24 @@ public class HelloController {
         return "hello-template";
     }
     @GetMapping("hello-string")
-    @ResponseBody
+    @ResponseBody  // http 헤데와 바디부분에서 직접넣어주겠다.
     public String helloString(@RequestParam("name")String name){
         return "hello"+name;  //"hello String"
      }
-
-
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name){
+        Hello hello = new Hello();  // 객체를 리턴하면 데이터 형태가  제이슨으로 반환된다. 단축키 command +shift +enter
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
