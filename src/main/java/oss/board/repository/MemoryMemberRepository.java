e7d26843ca1fd999d2b355b09ecc866b2211c9e3
ohsangsoo <tkdtn3264@gmail.com>
2023-03-02 18:25:48 +0900

    간단한 회원 아이디와 이름 통해 조회 기능 만들기 테스트 로확인하기

---
+++ src/main/java/oss/board/repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package oss.board.repository;
+
+import oss.board.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements  MemberRepository {
+    private static Map<Long,Member>store =new HashMap<>();
+    private  static long sequence =0L;
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(),member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+       return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+
+    @Override
+    public List<Member> findAlL() {
+        return new ArrayList<>(store.values());
+    }
+}
---
+++ src/test/java/oss/board/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,36 @@
+package oss.board.repository;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import oss.board.domain.Member;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+    MemberRepository repository =new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member =new Member();
+        member.setName("spring");
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+
+    }
+    @Test
+    public void findByName(){
+        Member member1 =new Member();
+        member1.setName("spring");
+        repository.save(member1);
+
+        Member member2 =new Member();
+        member2.setName("spring");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+        assertThat(result).isEqualTo(member1);
+    }
+}
