f80d5e3d6b2fc7abd2f17b8f9a6c46ef50c08c07
ohsangsoo <tkdtn3264@gmail.com>
2023-03-06 16:47:21 +0900

    회원 서비스 클래스  테스트 만들기 작업 완료

--- src/test/java/oss/board/service/MemberServiceTest.java
+++ src/test/java/oss/board/service/MemberServiceTest.java
@@ -1,47 +1,47 @@
 package oss.board.service;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
 import oss.board.domain.Member;
-
-import java.util.Optional;
-
+import oss.board.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class MemberServiceTest {
-    MemberService memberService = new MemberService();
-    @Test
-    void 회원가입() {
-        // given
-        Member member = new Member();
-        member.setName("hello");
-        //when
-        Long saveId =memberService.join(member);
-        //than
-        Member findMember = memberService.findOne(saveId).get();
-        assertThat(member.getName()).isEqualTo(findMember.getName());
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
     }
     @Test
-    public void 중복_회원_예외(){
-        //given
-        Member member1=new Member();
+    public void 회원가입() throws Exception {
+//Given
+        Member member = new Member();
+        member.setName("hello");
+//When
+        Long saveId = memberService.join(member);
+//Then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
+    }
+    @Test
+    public void 중복_회원_예외() throws Exception {
+//Given
+        Member member1 = new Member();
         member1.setName("spring");
-
-        Member member2=new Member();
+        Member member2 = new Member();
         member2.setName("spring");
-
-        //when
+        //When
         memberService.join(member1);
-        assertThrows(IllegalStateException.class,()->memberService.join(member2));
-//        try{
-//            memberService.join(member2);
-//            //fail();
-//        }catch(IllegalStateException e){
-//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-//
-//        }
-        //than
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다. assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
 
 
