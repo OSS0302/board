01a0fca7c1eadebd10fab37566c28f1e97c2a1b1
ohsangsoo <tkdtn3264@gmail.com>
2023-03-08 15:41:06 +0900

    회원웹 기능 - 홈화면 추가하기

--- src/main/java/oss/board/controller/MemberController.java
+++ src/main/java/oss/board/controller/MemberController.java
@@ -2,13 +2,30 @@ package oss.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import oss.board.domain.Member;
 import oss.board.service.MemberService;
 
 @Controller
 public class MemberController {
     private final MemberService memberService;
-    @Autowired //스프링부트 컨트롤러와 memberService 가 자동으로 연결하는 어노테이ㄹ
+    @Autowired //스프링부트 컨트롤러와 memberService 가 자동으로 연결하는 어노테이션 된다.
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MembersForm form){
+        Member member =new Member();
+        member.setName(form.getName());
+
+        System.out.println("member.getName() = " + member.getName());
+        memberService.join(member);
+        return "redirect:/";
+
     }
 }
---
+++ src/main/java/oss/board/controller/MembersForm.java
@@ -0,0 +1,13 @@
+package oss.board.controller;
+
+public class MembersForm {
+    private  String  name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
